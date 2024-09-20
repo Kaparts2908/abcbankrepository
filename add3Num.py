4bd4dae977308ccfd818a98faaa9c66ab5c0fbe0
unknown <Rachana@LAPTOP-MRM2B0L3>
2024-09-20 17:33:46 +0530

    add

---
+++ add3Num.py
@@ -0,0 +1,11 @@
+
+# Get user input
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Convert inputs to integers
+sum = int(num1) + int(num2)
+
+# Print the result
+print("The sum is:", sum)
+
